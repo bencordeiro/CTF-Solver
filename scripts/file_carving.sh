1c2a6c2a541595f152beef74dcb93c32b50880ac
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2022-05-21 22:54:28 -0400

    Update file_carving.sh

--- scripts/file_carving.sh
+++ scripts/file_carving.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 file $flag_file
-		echo "File Carving / Running Foremost"
-		foremost $flag_file
-		chmod -R +rwx output/
-		ZIP=output/zip
-		if [ -d "$ZIP" ]; then
-			echo "$ZIP is a directory."
-			echo "High probability of files hidden inside $flag_file!"
-			echo
-			echo "Please see $(pwd)/$ZIP for more info."
-		fi
+	echo "File Carving / Running Foremost"
+	foremost $flag_file
+	chmod -R +rwx output/
+	ZIP=output/zip
+	if [ -d "$ZIP" ]; then
+		echo "$ZIP is a directory."
+		echo "High probability of files hidden inside $flag_file!"
+		echo
+		echo "Please see $(pwd)/$ZIP for more info."
+	fi
