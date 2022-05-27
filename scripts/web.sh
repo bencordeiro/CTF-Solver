8cb7fa21abddce757c76cda362095aca8f4c0133
bencordeiro <94664097+bencordeiro@users.noreply.github.com>
2022-05-27 12:03:57 -0400

    Update web.sh

--- scripts/web.sh
+++ scripts/web.sh
@@ -6,7 +6,6 @@
 
 func() {
 echo
-    read -p "What is the keyword?(input)" var_word
     read -p "What is the url?(input)" var_url
     wget -m $var_url
     var_url1=$(echo $var_url | cut -c 8- | rev | cut -c2- | rev)
@@ -20,7 +19,14 @@ GET=y
     [ -n "$var_get" ] && GET=$var_get
 
 if [ $GET = y ]; then
+	read -p "What is the keyword?(input)" var_word
 	func
+fi
+########### Check for robots.txt ###########
+var_bot="robots.txt"
+
+if curl $var_url/robots.txt | grep User-agent || curl $var_url$var_bot; then
+    echo "You should look in robots.txt"
 else
-	exit
+    echo "Nothing found in robots.txt"
 fi
